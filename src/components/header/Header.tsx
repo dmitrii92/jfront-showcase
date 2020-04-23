57c7bcfa427ac56518c35d83b1da87978ce0f3bc
dmitrii92 <fogot92@gmail.com>
2020-04-23 16:37:10 +0300

    Update styles.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,12 +4,16 @@ import styled from "styled-components";
 const Header = styled.header`
   display: inline-block;
   margin-left: 2px;
-  padding: 1px 6px 0;
   margin-bottom: -1px;
+  padding: 1px 6px 1px;  
   border: 1px solid #8DB2E3;
+  border-bottom-color: #D7E4F3;
   background: #D7E4F3;
   color: rgb(21, 66, 139);
   font-weight: bold;
+  font-size: 13px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
 `;
 
 export default Header;
