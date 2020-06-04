2c37d1926617717abc0d1631e17381abd70d3856
dmitrii92 <fogot92@gmail.com>
2020-06-04 18:50:55 +0300

    Fix.

--- src/App.tsx
+++ src/App.tsx
@@ -39,25 +39,22 @@ function Main() {
   }, [language])
 
   return (
-      <>
-        <h1>{t("feature.header")}</h1>
-        <SearchContext.Provider value={searchFeature}>
-          <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
-            <Switch>
-              <Route path="/" exact component={SearchPage}/>
-              <Route path="/create" exact component={CreatePage}/>
-              <Route path="/:featureId/edit" exact component={EditPage}/>
-              <Route path="/:featureId/detail" component={DetailPage}/>
-              <Route path="/list/:searchId" component={ListPage}/>
-              <Route path="/:featureId/feature-process" exact component={FeatureProcessListPage}/>
-              <Route path="/:featureId/feature-process/:featureProcessId/detail"
-                     component={FeatureProcessDetailPage}/>
-              <Route path="/:featureId/feature-process/create" exact
-                     component={FeatureProcessCreatePage}/>
-            </Switch>
-          </BrowserRouter>
-        </SearchContext.Provider>
-      </>
+      <SearchContext.Provider value={searchFeature}>
+        <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
+          <Switch>
+            <Route path="/" exact component={SearchPage}/>
+            <Route path="/create" exact component={CreatePage}/>
+            <Route path="/:featureId/edit" exact component={EditPage}/>
+            <Route path="/:featureId/detail" component={DetailPage}/>
+            <Route path="/list/:searchId" component={ListPage}/>
+            <Route path="/:featureId/feature-process" exact component={FeatureProcessListPage}/>
+            <Route path="/:featureId/feature-process/:featureProcessId/detail"
+                   component={FeatureProcessDetailPage}/>
+            <Route path="/:featureId/feature-process/create" exact
+                   component={FeatureProcessCreatePage}/>
+          </Switch>
+        </BrowserRouter>
+      </SearchContext.Provider>
   );
 }
 
